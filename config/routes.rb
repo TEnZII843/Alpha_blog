375776f193b11e54d1a1b209249ca7109aa564a9
KT <TEnZII843>
2023-07-17 13:43:20 +0600

    feat:user-signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   root "articles#home"
-  get "/about", to:"articles#about"
-
+  get "about", to:"articles#about"
+  resources :articles
+  get 'signup', to:"users#new"
+  # post 'users', to:'users#create'
   # resources :articles do
   #   resources :comments
   # end
-  resources :articles
+  resources :users, except: [:new]
 end
